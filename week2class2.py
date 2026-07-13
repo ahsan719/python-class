73a40f6857c26a4087438e46bc233063f70acd1d
ahsan719 <ahsannilore@gmail.com>
2026-07-13 10:33:09 +0500

    week2class2

--- week2class2.py
+++ week2class2.py
@@ -3,13 +3,15 @@
 # def greet(name, age):  #parameter
 #     print("Hello", name, age)
 
-
 # greet("Ahsan", 22) 
 # greet("Ali", 21)  #Arguements
 
+
+
+
+
 # def add(a,b):
 #    return a+b
-
 
 # result1=add(10,20)
 # result2=add(30,50)
@@ -18,11 +20,17 @@
 
 
 
+
+
+
 # def table(number):
 #     for i in range(1,11):
 #         print(number, "*", i , "=", number*i)
 
 # table(8)
+
+
+
 
 def check_pass(marks):
     if marks>50:
